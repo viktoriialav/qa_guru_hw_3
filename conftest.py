1aed1f506072861bd43d943803b1695345961cfb
Viktoriia Lavrova <viktoriia.lav2908@gmail.com>
2024-06-22 00:46:46 +0300

    Update conftest.py and test_google_search.py

--- conftest.py
+++ conftest.py
@@ -1,16 +1,11 @@
 import pytest
 from selene import browser
-from selene.support import webdriver
 
 
 @pytest.fixture(autouse=True)
 def browser_options():
-    browser.config.base_url = 'https://www.google.ru/'
-    browser.config.window_height = 600
-    browser.config.window_width = 800
-
-    driver_options = webdriver.ChromeOptions()
-    driver_options.page_load_strategy = 'eager'
-
+    browser.config.base_url = "https://google.com"
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
     yield
     browser.quit()
--- test_google_search.py
+++ test_google_search.py
@@ -1,16 +1,15 @@
-import pytest
 from selene import browser, have
 
 
 def test_google_search_positive():
     browser.open('')
     browser.element('[name=q]').type('Capital of Uruguay').press_enter()
-    browser.element('[class=xpdopen]').should(have.text('Montevideo. '))
+    browser.element('[class=kp-header]').should(have.text('Монтевидео'))
 
 
 def test_google_search_negative():
     browser.open('')
     text = 'dfstetgtgtgtgtgtgtvb2363kikikik'
     browser.element('[name=q]').type(text).press_enter()
-    browser.element('[class=kp-header]').should(have.text(f'По запросу {text} ничего не найдено. '))
+    browser.element('[class=card-section]').should(have.text(f'По запросу {text} ничего не найдено. '))
 
